4f1ed73cbdf1c59a72791ebfe9bc01f36de2b4da
AyOULAARBI <ayoub6oulaarbi6@gmail.com>
2023-01-17 21:54:57 +0100

    I added the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-
+import { Routes,Route } from "react-router-dom";
+import Home from "./components/Home";
+import Pomodor from "./components/Pomodor";
+import Settings from "./components/Settings";
 
 
 function App() {
 
   return (
-    <h1>initial commit</h1>
+    <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/pomodoro" element={<Pomodor/>} />
+    <Route path="/settings" element={<Settings/>} />
+    </Routes>
   )
 }
 
