90de9ac20047c75ec220cefcd35c50700181e0f7
AyOULAARBI <ayoub6oulaarbi6@gmail.com>
2023-01-17 22:05:42 +0100

    I changed the routing  from the old Route and Routes components to the useRoutes Hook,because WHY NOT hahaha

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { Routes,Route } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
 import Home from "./components/Home";
 import Pomodor from "./components/Pomodor";
 import Settings from "./components/Settings";
 
 
 function App() {
+  const elements = useRoutes([
+    {path:"/", element:<Home/>},
+    {path:"/pomodoro" ,element:<Pomodor/>},
+    {path:"/settings" ,element:<Settings/>}
+  ])
 
-  return (
-    <Routes>
-    <Route path="/" element={<Home/>} />
-    <Route path="/pomodoro" element={<Pomodor/>} />
-    <Route path="/settings" element={<Settings/>} />
-    </Routes>
+  return (elements
   )
 }
 
