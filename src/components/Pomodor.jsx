1a6555e4f69a52777524e8fa443bfc35886bc4cd
AyOULAARBI <ayoub6oulaarbi6@gmail.com>
2023-01-18 23:09:04 +0100

    added different types of timers

--- src/components/Pomodor.jsx
+++ src/components/Pomodor.jsx
@@ -1,14 +1,15 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import React,{ useState,useEffect } from "react";
 
-function Pomodor() {
-  const [max,setMax] = useState(5)
+function Pomodor({periods}) {
+  const [current,setCurrent] = useState('sprint')
   const [minutes,setMinutes] = useState(0);
   const [seconds,setSeconds] = useState(0);
+  let curr = periods.find(elm=>elm.type===current);  
+    console.log(curr)
   var timer;
-    useEffect(() => {
-      timer = setInterval(() => {
-        if(minutes != max){
+  const startTimer = () => {
+    timer = setInterval(() => {
+        if(minutes != curr.minutes){
           setSeconds(seconds+1);
             if(seconds ===59){
               setMinutes(minutes+1)
@@ -18,11 +19,25 @@ function Pomodor() {
         clearInterval(timer)
       }
       }, 1000);
-      return ()=>clearInterval(timer)
-    });
-  
+      
+    };
+    const stopTimer = ()=>{
+      clearInterval(timer)
+    }
     return(<div className="container">
+        <span>
+          <button onClick={()=>setCurrent('sprint')}>sprint</button>
+          <button onClick={()=>setCurrent('short break')}>short break</button>
+          <button onClick={()=>setCurrent('long break')}>long break</button>
+        </span>
+        <div>
         <h1>{minutes<10?`0${minutes}`:minutes}:{seconds<10?`0${seconds}`:seconds}</h1>
+        {curr.minutes && <h2>timer is set to : {curr.minutes} minutes</h2>}
+        </div>
+        <span>
+        <button onClick={()=>startTimer()}>Start</button>
+        <button onClick={()=>stopTimer()}>Stop</button>
+        </span>
         </div>
   )
 }
