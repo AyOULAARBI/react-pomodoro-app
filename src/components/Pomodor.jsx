c5059811f53a9117692706a5fd62461d0cebb8f5
AyOULAARBI <ayoub6oulaarbi6@gmail.com>
2023-01-18 21:20:09 +0100

    after a lot of trail and error,finally figured out how to set up the timer

--- src/components/Pomodor.jsx
+++ src/components/Pomodor.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useState } from "react";
 
 function Pomodor() {
-  return (
-    <div>Pomodor</div>
+  const [max,setMax] = useState(5)
+  const [minutes,setMinutes] = useState(0);
+  const [seconds,setSeconds] = useState(0);
+  var timer;
+    useEffect(() => {
+      timer = setInterval(() => {
+        if(minutes != max){
+          setSeconds(seconds+1);
+            if(seconds ===59){
+              setMinutes(minutes+1)
+              setSeconds(0)
+            }
+      }else{
+        clearInterval(timer)
+      }
+      }, 1000);
+      return ()=>clearInterval(timer)
+    });
+  
+    return(<div className="container">
+        <h1>{minutes<10?`0${minutes}`:minutes}:{seconds<10?`0${seconds}`:seconds}</h1>
+        </div>
   )
 }
 
