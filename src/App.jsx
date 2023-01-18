bc2ec5803dac24895a3a010060f581fd7aa8075f
AyOULAARBI <ayoub6oulaarbi6@gmail.com>
2023-01-18 23:09:34 +0100

    modified:   src/App.css 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { Routes,Route } from "react-router-dom";
+// import { Routes,Route } from "react-router-dom";
 import { useRoutes } from "react-router-dom";
+import { useState } from "react";
 import Home from "./components/Home";
 import Pomodor from "./components/Pomodor";
 import Settings from "./components/Settings";
 import './App.css'
 
 function App() {
+  let periods = [
+    {type:"sprint",minutes:25},
+    {type:"short break",minutes:5},
+    {type:"long break",minutes:15},
+  ]
+  const [period,setPeriod] = useState(periods)
   const elements = useRoutes([
     {path:"/", element:<Home/>},
-    {path:"/pomodoro" ,element:<Pomodor/>},
-    {path:"/settings" ,element:<Settings/>}
+    {path:"/pomodoro" ,element:<Pomodor periods={period}/>},
+    {path:"/settings" ,element:<Settings setMax={setPeriod} />}
   ])
 
   return (elements
